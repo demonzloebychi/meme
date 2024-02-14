8ce764be6ea247c6f2710b1b05149f27b0687e92
Nikolai <107766413+demonzloebychi@users.noreply.github.com>
2024-02-15 02:17:09 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const audio1 = new Audio();
+audio1.src = "/music/win-close.mp3";
+
+const audio2 = new Audio();
+audio2.src = "/music/win-open.mp3";
+
+const audio3 = new Audio();
+audio3.src = "/music/win-error.mp3";
+
+const audio4 = new Audio();
+audio4.src = "/music/oh-my-god.mp3";
